87f093f2723329d808e2556337a19c14e3ef1d0d
hjj5612 <hjj5612@naver.com>
2021-12-25 22:11:52 +0900

    문제 1010번 추가

---
+++ src/p1904/Main.java
@@ -0,0 +1,35 @@
+package p1904;
+
+import java.util.Scanner;
+
+/*
+https://www.acmicpc.net/problem/1904
+ */
+public class Main {
+    private static long[] dp;
+
+    private static long solve(int n) {
+        dp = new long[n + 1];
+
+        for(int i = 0; i < n + 1; i++) {
+            dp[i] = -1;
+        }
+
+        return f(n);
+    }
+
+    private static long f(int n) {
+        if(dp[n] != -1) return dp[n];
+
+        if(n == 1) return dp[n] = 1;
+        else if(n == 2) return dp[n] = 2;
+        else return dp[n] = (f(n - 1) + f(n - 2)) % 15746;
+    }
+
+    public static void main(String[] args) {
+        try(Scanner kb = new Scanner(System.in)) {
+            int n = kb.nextInt();
+            System.out.println(solve(n));
+        }
+    }
+}
