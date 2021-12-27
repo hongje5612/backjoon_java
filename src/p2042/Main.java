56ab0de81403630b9d24fa5d563c4313977d4e2f
hjj5612 <49805380+hongje5612@users.noreply.github.com>
2021-12-28 08:04:41 +0900

    2042번 추가

--- src/p2042/Main.java
+++ src/p2042/Main.java
@@ -1,6 +1,33 @@
 package p2042;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    /**
+     * 밑 수가 2 인 log 값
+     * @param n
+     * @return
+     * 밑 수가 2 인 log 값, 소수점 밑으로 버린 값을 반환합니다.
+     */
+    private static int log2(int n) {
+        if(n == 1) return 0;
+
+        int x = 2;
+
+        for(int answer = 1; true; answer++) {
+            int x2 = x * 2;
+
+            if(n >= x && n < x2) return answer;
+
+            x = x2;
+        }
+    }
+
     private static class SegmentTree {
         private long[] tree;    //complete binary tree
         private int size;       //size of array of tree
@@ -8,17 +35,19 @@ public class Main {
         private int sizeOfNodesInLastLevelOfFull;     // full binary tree
 
         private long[] arr; // tempary used
-        
+
         public SegmentTree(long[] arr) {
-            int level = (int)Math.log(arr.length);
+            int level = log2(arr.length);
             int sizeOfNodes = (int)Math.pow(2, level);
 
             // if full binary tree
             if(sizeOfNodes == arr.length) {
-                tree = new long[sizeOfNodes * 2 - 1];
+                size = sizeOfNodes * 2 - 1;
+                tree = new long[size];
                 sizeOfNodesInLastLevelOfFull = sizeOfNodes;
             } else {
-                tree = new long[sizeOfNodes * 2 - 1 + arr.length];
+                size = sizeOfNodes * 2 - 1 + arr.length;
+                tree = new long[size];
                 sizeOfNodesInLastLevelOfFull = sizeOfNodes * 2;
             }
 
@@ -39,37 +68,107 @@ public class Main {
         }
 
         private void update(int node, int left, int right, int index, long diff) {
-            if(index >= sizeOfNodesInLastLevelOfComplete) return;
+            if(index < left || index > right) return;
 
-            if(left == right) {
-                tree[node] += diff;
-                return;
-            } else {
-                if(index >= left && index <= right) {
-                    tree[node] += diff;
-                    int half = (left + right) / 2;
-                    update(node * 2 + 1, left, half, index, diff);
-                    update(2 * (node + 1), half + 1, right, index, diff);
-                }
+            tree[node] += diff;
+
+            if(left != right) {
+                int half = (left + right) / 2;
+                update(node * 2 + 1, left, half, index, diff);
+                update(2 * (node + 1), half + 1, right, index, diff);
             }
+        }
+
+        public void update(int index, long value) {
+            long diff = value - arr[index];
+            update(0, 0, sizeOfNodesInLastLevelOfFull - 1, index, diff);
         }
 
         private long sum(int node, int left, int right, int start, int end) {
+            if(left > end || right < start) return 0;
+            if(left >= start && right <= end) return tree[node];
+            else {
+                int half = (left + right) / 2;
+                long a = sum(node * 2 + 1, left, half, start, end);
+                long b = sum(2 * (node + 1), half + 1, right, start, end);
+                return Math.addExact(a, b);
+            }
+        }
 
+        public long sum(int start, int end) {
+            return sum(0, 0, sizeOfNodesInLastLevelOfFull - 1, start, end);
         }
 
         public void show() {
+            System.out.println(size);
             for(long t : tree) {
                 System.out.print(t + " ");
             }
+            System.out.println();
         }
     }
 
-    public static void main(String[] args) {
-        long[] arr = { 1, 2, 3, 4 };
+    public static class Command {
+        public byte a;
+        public int b;
+        public long c;
 
-        SegmentTree tree = new SegmentTree(arr) ;
-        tree.show();
+        public Command(byte a, int b, long c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
     }
 
+    private static long[] arr;
+    private static List<Command> commandList;
+
+    private static boolean input() {
+        boolean state;
+
+        try(BufferedReader kb = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] ss = kb.readLine().split("\\s+");
+
+            int n = Integer.parseInt(ss[0]);
+            int m = Integer.parseInt(ss[1]);
+            int k = Integer.parseInt(ss[2]);
+
+            arr = new long[n];
+            commandList = new ArrayList<>();
+
+            for(int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(kb.readLine());
+            }
+
+            for(int i = 0; i < m + k; i++) {
+                ss = kb.readLine().split("\\s+");
+
+                commandList.add(new Command(Byte.parseByte(ss[0]), Integer.parseInt(ss[1]), Long.parseLong(ss[2])));
+            }
+            state = true;
+        } catch (IOException e) {
+            state = false;
+        } catch (Exception e) {
+            state = false;
+        }
+        return state;
+    }
+
+
+    public static void main(String[] args) {
+        if(input()) {
+            SegmentTree tree = new SegmentTree(arr);
+
+            for(Command command : commandList) {
+                switch(command.a) {
+                    case 1 :
+                        tree.update(--command.b, command.c);
+                        break;
+                    case 2 :
+                        System.out.println(tree.sum(--command.b, (int)--command.c));
+                        break;
+                }
+            }
+        }
+    }
 }
